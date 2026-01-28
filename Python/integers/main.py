97593b045dec34a75dda38efdc394422844a7774
Sora Plaisance <soraplaisance06@gmail.com>
2026-01-27 20:10:15 -0600

    integers

---
+++ Python/integers/main.py
@@ -0,0 +1,27 @@
+milk = 5
+eggs = 3
+coffee = 10
+
+total = milk + eggs + coffee
+discount = total * 0.1
+final_total = total - discount
+print(total)
+print(discount)
+print(final_total)
+
+# Final Challenge
+
+first_num = int(input("Enter the first integer: "))
+second_num = int(input("Enter the second integer: "))
+
+add = first_num + second_num
+subtract = first_num - second_num
+multiply = first_num * second_num
+divide = first_num / second_num
+divide_floor = first_num // second_num
+
+print("Addition: ", add)
+print("Subtraction: ", subtract)
+print("Multiplication: ", multiply)
+print("Division: ", divide)
+print("Floor Division: ", divide_floor)
